4d3d4bacf9e2f2f9f6552abed304a087fcda2e5f
abhigitit <90536339+abhigitit@users.noreply.github.com>
2022-02-25 11:04:57 -0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 const loadRandomName = (resultDiv) => {
   console.log("inside clic");
-  fetch("http://52.15.70.208:5000/random-name")
+  fetch("http://18.223.44.5:5001/random-name")
     .then((response) => response.json())
     .then((result) => {
       resultDiv.classList.add("alert", "alert-success");
